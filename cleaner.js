b167e1366a1426ce05aae8047abd627b1634f35b
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2019-12-06 12:04:29 -0300

    Adds fallback when social network does not exists

--- cleaner.js
+++ cleaner.js
@@ -1,11 +1,27 @@
-document.querySelector('h1').style.borderBottom = "3px solid black";
-document.querySelectorAll('h2').forEach((el) => el.style.borderBottom = "3px solid black");
-document.querySelectorAll('a').forEach((el) => {
-    el.style.color = "black";
-    el.style.textDecoration = "none";
-    el.style.background = "none";
+function getElementByXpath(path) {
+  return document.evaluate(
+    path,
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue;
+}
+
+document.querySelector("h1").style.borderBottom = "3px solid black";
+document
+  .querySelectorAll("h2")
+  .forEach(el => (el.style.borderBottom = "3px solid black"));
+document.querySelectorAll("a").forEach(el => {
+  el.style.color = "black";
+  el.style.textDecoration = "none";
+  el.style.background = "none";
 });
 
-document.querySelector('#social-networks').style.marginTop = "30px";
-document.querySelector('footer').style.visibility = "hidden";
-document.querySelector('hr').style.visibility = "hidden";
+const socialNetworks = document.querySelector("#social-networks");
+const footer = document.querySelector("footer");
+const hr = (document.querySelector("hr").style.visibility = "hidden");
+
+socialNetworks && (socialNetworks.style.marginTop = "30px");
+footer && (footer.style.visibility = "hidden");
+hr && (hr.style.visibility = "hidden");
